2eccfbeb1cf6f8a5cd78d0c3286bd6c7ea640435
akiehl2000 <akiehl2000@gmail.com>
2022-03-27 16:23:59 -0600

    Fixed batch request script

--- fitINGARCH.sh
+++ fitINGARCH.sh
@@ -1,12 +1,16 @@
 #! /bin/bash
 
-#SBATCH --nodes=1
-#SBATCH --time=00:05:00
-#SBATCH --ntasks=1
+#SBATCH --account=csu-general
+#SBATCH --qos normal
 #SBATCH --job-name=fitINGARCH
+#SBATCH --output=%x_%a_%j.out
+#SBATCH --error=%x_%a_%j.err
+#SBATCH --time=00:05:00
+#SBATCH --nodes=1
+#SBATCH --mail-user=adam.kiehl@colostate.edu
+#SBATCH --mail-type=ALL
 
-module purge
+source ~/../../curc/sw/anaconda/default
+conda activate fitEnv2
 
-module load anaconda
-
-Rscript ./Scripts/INGARCH_models.Rmd
+Rscript summitTest.R
