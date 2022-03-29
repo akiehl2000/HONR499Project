1e7c97c02fa6e6f2aff5caa0ae0ece61f2c8e55d
akiehl2000 <akiehl2000@gmail.com>
2022-03-29 17:17:35 -0600

    GLARMA batch script

---
+++ Batch/fitGLARMA.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+#SBATCH --account=csu-general
+#SBATCH --qos normal
+#SBATCH --job-name=fitGLARMA
+#SBATCH --output=../Output/%x_%a_%j.out
+#SBATCH --error=../Output/%x_%a_%j.err
+#SBATCH --time=05:00:00
+#SBATCH --nodes=1
+#SBATCH --mail-user=adam.kiehl@colostate.edu
+#SBATCH --mail-type=ALL
+
+source ~/../../curc/sw/anaconda/default
+conda activate fitEnv2
+
+Rscript ../Scripts/GLARMA_models.R
