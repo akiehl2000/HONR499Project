0d6e9af70f8a9d7d02fd9a1985ed3406b36ef5f5
akiehl2000 <akiehl2000@gmail.com>
2022-03-29 18:10:52 -0600

    Increase batch request max time

--- Batch/fitGLAR.sh
+++ Batch/fitGLAR.sh
@@ -3,9 +3,9 @@
 #SBATCH --account=csu-general
 #SBATCH --qos normal
 #SBATCH --job-name=fitGLAR
-#SBATCH --output=../Output/%x_%a_%j.out
-#SBATCH --error=../Output/%x_%a_%j.err
-#SBATCH --time=05:00:00
+#SBATCH --output=../Output/GLAR/%x_%a_%j.out
+#SBATCH --error=../Output/GLAR/%x_%a_%j.err
+#SBATCH --time=48:00:00
 #SBATCH --nodes=1
 #SBATCH --mail-user=adam.kiehl@colostate.edu
 #SBATCH --mail-type=ALL
--- Batch/fitINGARCH.sh
+++ Batch/fitINGARCH.sh
@@ -3,9 +3,9 @@
 #SBATCH --account=csu-general
 #SBATCH --qos normal
 #SBATCH --job-name=fitINGARCH
-#SBATCH --output=../Output/%x_%a_%j.out
-#SBATCH --error=../Output/%x_%a_%j.err
-#SBATCH --time=05:00:00
+#SBATCH --output=../Output/INGARCH/%x_%a_%j.out
+#SBATCH --error=../Output/INGARCH/%x_%a_%j.err
+#SBATCH --time=48:00:00
 #SBATCH --nodes=1
 #SBATCH --mail-user=adam.kiehl@colostate.edu
 #SBATCH --mail-type=ALL
